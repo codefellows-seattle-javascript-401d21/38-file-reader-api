a5eb63a21cff8ae5b79e2b8a5f7ac3713dba9442
Ed Abrahamsen <esabrahamsen@yahoo.com>
2018-03-07 14:58:21 -0800

    add picture form

--- lab-ed/cfgram-frontend/src/component/content/index.js
+++ lab-ed/cfgram-frontend/src/component/content/index.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import PictureForm from '../../component/picture-form'
+import * as pictureActions from '../../action/picture-action'
 
-export default class Content extends React.Component {
+class Content extends React.Component {
   render() {
     return (
       <div>
         <h1>Welcome to the photo app</h1>
-        <p>{this.props.token}</p>
+        <PictureForm onComplete={this.props.createPicture}/>
+        {/* <p>{this.props.token}</p> */}
       </div>
     )
   }
 }
+
+const mapDispatchToProps = dispatch => ({
+  createPicture : picture => dispatch(pictureActions.createAction(picture))
+})
+
+export default connect(null, mapDispatchToProps)(Content)
